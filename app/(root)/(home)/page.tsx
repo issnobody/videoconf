7bbd11905e97b34219e2de4eee4663fd4afe6d46
Ayush <aayushsharma33.as@gmail.com>
2024-08-29 13:58:50 +0545

    timmezone

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,11 @@
+"use client";
 import MeetingTypeList from '@/components/MeetingTypeList';
+import { Client } from '@clerk/nextjs/server';
 
 const Home = () => {
   const now = new Date();
 
-  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', timeZone: 'Asia/Kathmandu' });
+  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit'});
   const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
 
   return (
